b244be2c146e36dccfdda88ff7719d24aeb6de0c
yinhao131415 <huanhao1317@163.com>
2026-06-09 13:49:04 +0800

    设置控制设备

--- hydros_agent_sdk/agent_commands/models/device_value_types.py
+++ hydros_agent_sdk/agent_commands/models/device_value_types.py
@@ -10,12 +10,12 @@ from enum import Enum
 class DeviceValueTypeEnum(Enum):
     """贴近 Java 的设备值类型定义。"""
 
-    WATER_LEVEL = ("WATER_LEVEL", "水位", float)
-    GATE_OPENING = ("GATE_OPENING", "闸门开度", float)
-    BLADE_ANGLE = ("BLADE_ANGLE", "叶片角度", float)
-    OUTPUT_POWER = ("OUTPUT_POWER", "输出功率", float)
-    UNIT_STATUS = ("UNIT_STATUS", "机组启停状态", int)
-    UNIT_OPENING = ("UNIT_OPENING", "机组开度", float)
+    WATER_LEVEL = ("water_level", "水位", float)
+    GATE_OPENING = ("gate_opening", "闸门开度", float)
+    BLADE_ANGLE = ("blade_angle", "叶片角度", float)
+    OUTPUT_POWER = ("output_power", "输出功率", float)
+    UNIT_STATUS = ("unit_status", "机组启停状态", int)
+    UNIT_OPENING = ("unit_opening", "机组开度", float)
 
     def __init__(self, code: str, label: str, value_type: type):
         self.code = code
